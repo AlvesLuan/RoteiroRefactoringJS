5b7c5a34f19a0c9de7ddf1734eb466222d40e17d
Luan Santos <luankauan10@gmail.com>
2025-07-27 16:06:53 -0300

    Commit 2 - Replace Temp with Query

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const { readFileSync } = require('fs');
 
 function gerarFaturaStr (fatura, pecas) {
-    function calcularTotalApresentacao(apre, peca) {
+	function getPeca(apresentacao) {
+  		return pecas[apresentacao.id];
+	}
+
+    function calcularTotalApresentacao(apre) {
+		const peca = getPeca(apre);
 		let total = 0;
 
 		switch (peca.tipo) {
@@ -34,23 +39,19 @@ function gerarFaturaStr (fatura, pecas) {
                             minimumFractionDigits: 2 }).format;
   
     for (let apre of fatura.apresentacoes) {
-    	const peca = pecas[apre.id];
-      	let total = calcularTotalApresentacao(apre, peca);
-  
-      
-      
-      
+
+      	let total = calcularTotalApresentacao(apre);
   
 		// créditos para próximas contratações
 		creditos += Math.max(apre.audiencia - 30, 0);
-		if (peca.tipo === "comedia") 
+		if (getPeca(apre).tipo === "comedia") 
 			creditos += Math.floor(apre.audiencia / 5);
 	
 		// mais uma linha da fatura
-		faturaStr += `  ${peca.nome}: ${formato(total/100)} (${apre.audiencia} assentos)\n`;
+		faturaStr += `  ${getPeca(apre).nome}: ${formato(total/100)} (${apre.audiencia} assentos)\n`;
 		totalFatura += total;
     }
-	
+
     faturaStr += `Valor total: ${formato(totalFatura/100)}\n`;
     faturaStr += `Créditos acumulados: ${creditos} \n`;
     return faturaStr;
